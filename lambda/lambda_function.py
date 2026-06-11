4d760f7ed084811c95330bad158df4f2167f3e4d
Mohamed Yahye <yahye.mohamed.gt@gmail.com>
2026-06-11 17:13:16 -0400

    updated attempt 1

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from our cicd github workflow! from vscode')
+        'body': json.dumps('Hello from our cicd gihub workflow! from vscode')
     }
